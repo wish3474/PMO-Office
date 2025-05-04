19add33eccb45b0eb335fe32fe269554c8c4286d
wish3474 <2021wa86947@wilp.bits-pilani.ac.in>
2025-05-04 23:16:12 +0530

    Version 2 update in Java branch

--- TestScript.java
+++ TestScript.java
@@ -1,1 +1,1 @@
-public class TestScript {\n  public static void main(String[] args) {\n    System.out.println("Version 1 - Java code");\n  }\n}
+public class TestScript {\n  public static void main(String[] args) {\n    System.out.println("Version 2 - Java code update");\n  }\n}
